0060c91a4e0b00eff8b0394b3f6517b3c0235368
heylucky <linyue219@126.com>
2017-01-20 15:58:53 +0800

    django默认只有用户名登录，如何自定义登录选项，如使用Email可以登录

--- MXOnline/settings.py
+++ MXOnline/settings.py
@@ -30,7 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTHENTICATION_BACKENDS = (
+    'users.views.CustomBackend',
+)
 # Application definition
 
 INSTALLED_APPS = [
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,20 @@
 # _*_ coding:utf-8 _*_
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 
+from models import UserProfile
+
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user=UserProfile.objects.get(Q(username=username)|Q(email=username))        #用户名或邮件登录
+            if user.check_password(password):       # 检查明文暗纹密码是否一样
+                return user
+        except Exception as e:
+            return None
 
 def user_login(request):
     if request.method == 'POST':
@@ -13,7 +26,7 @@ def user_login(request):
             login(request,user)                                         # 完成login登录,下面跳到首页
             return render(request,"index.html",{})
         else:
-            return render(request,"login.html",{})
+            return render(request,"login.html",{'msg': '用户名或密码错误！'})
     elif request.method == 'GET':
         return render(request,'login.html',{})
 
